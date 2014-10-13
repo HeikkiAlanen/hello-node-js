e293c7ad9ffaba033ff55c6aa940a5c5bcfa19a4
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-10-13 13:49:08 +0300

    Tests run with Grunt. Lecture 7, task 2.

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+/*
+	Grunt needs to be installed globally before grunt can be used. (`npm install -g grunt`)
+	Linting from command line with grunt: `grunt --gruntfile=grunt-conparison.js`
+	Grunt executes 'default'-task which is defined as eslint.
+*/
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+		eslint: {
+			options: {
+				config: 'eslintRulesHAl.json'
+			},
+			target: ['*.js']
+		},
+		nodeunit: {
+			all: ['./test/*_spec.js']
+		}
+	});
+  
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('test', ['nodeunit']);
+  };
