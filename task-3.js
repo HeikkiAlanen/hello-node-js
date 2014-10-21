893d252d850b2df23e40cf0a192da820978a96d2
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-10-21 17:32:12 +0300

    Datetime-module added for formatting dates.

--- task-3.js
+++ task-3.js
@@ -1,15 +1,22 @@
 var filu = process.argv[2];
 
 var m = require('./task-3-2.js');
+var dateFormat = require('./node_modules/dateformat');
 
 var timestamp = m(filu);
 if (timestamp !== false) {
-	var date = new Date(timestamp);	
+	var date = new Date(timestamp);
+	
+	dateFormat.masks.finTime = 'dd.mm.yyyy HH.MM';
+    console.log(dateFormat(date, "finTime"));
+	
+	/*
 	var fin = date.getDate() + '.' + 
 	(date.getMonth() + 1) + '.' +
 	date.getFullYear();
 	fin += ' ' + date.getHours() + '.' + date.getMinutes();
 	console.log(fin);
+	*/
 } else {
 	console.log("Tiedostoa ei löydy!");
 }
